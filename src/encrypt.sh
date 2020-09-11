879529a798b33f2dac224df6cc00ce96de53881f
Anton Mrosek <github@anjomro.de>
2020-09-11 20:40:46 +0200

    Change Location of configuration files

--- src/encrypt.sh
+++ src/encrypt.sh
@@ -6,7 +6,7 @@ echo -ne "\n"
 openssl enc -aes-256-cbc -iter 1000000  -in $1 -out $1.enc -pass file:./key.bin
 echo "Encrypted $1, stored as $1.enc!"
 mv $1 ORIGINAL$1
-openssl rsautl -encrypt --inkey /thebackupper/public_key.pem -pubin -in key.bin -out key.bin.enc
+openssl rsautl -encrypt --inkey /public_key.pem -pubin -in key.bin -out key.bin.enc
 rm key.bin
 echo "Encrypted key.bin with public key as key.bin.enc, deleted key.bin."
 echo "Finished encryption"
--- src/start.sh
+++ src/start.sh
@@ -13,8 +13,8 @@ fi
 zip -r $nameprefix-backup.zip /backup/
 backupname=$nameprefix-backup.zip
 originalbackupname=$backupname
-if [[ -f "/thebackupper/public_key.pem" ]]; then
-	if grep "BEGIN PUBLIC KEY" /thebackupper/public_key.pem > /dev/null
+if [[ -f "/public_key.pem" ]]; then
+	if grep "BEGIN PUBLIC KEY" /public_key.pem > /dev/null
 	then
 		./encrypt.sh $backupname
 		backupname=$nameprefix-backup.zip.enc
@@ -28,15 +28,15 @@ if [[ -v $remotepath ]]; then
 fi
 
 if [[ -v remote ]]; then
-	if [[ -f "/thebackupper/remote.conf" ]]; then
+	if [[ -f "/remote.conf" ]]; then
 		echo "Uploading to $remote"
 		echo "#!/bin/bash" > decryptBackup.sh
 		echo "todecrypt=$originalbackupname" >> decryptBackup.sh
 		cat decrypt.sh >> decryptBackup.sh
 		chmod +x decryptBackup.sh
-		rclone --config=/thebackupper/remote.conf copy key.bin.enc $remote:$remotepath/$nameprefix/
-		rclone --config=/thebackupper/remote.conf copy $backupname $remote:$remotepath/$nameprefix/
-		rclone --config=/thebackupper/remote.conf copy decryptBackup.sh $remote:$remotepath/$nameprefix/
+		rclone --config=/remote.conf copy /thebackupper/key.bin.enc $remote:$remotepath/$nameprefix/
+		rclone --config=/remote.conf copy /thebackupper/$backupname $remote:$remotepath/$nameprefix/
+		rclone --config=/remote.conf copy /thebackupper/decryptBackup.sh $remote:$remotepath/$nameprefix/
 		echo "Upload finished!"
 	else
 		echo "No config file mounted!"
